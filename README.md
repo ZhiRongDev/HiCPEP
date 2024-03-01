97e253720c34ed0f2abb270fc8718c944d107b88
jordan990301 <jordan990301@gmail.com>
2024-03-01 10:45:48 +0800

    Rename to HiC-PAPP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hi-C Approximated PC1 Pattern (HiCAPP)
+# Hi-C Pearson matrix Approximated PC1 Pattern (HiC-PAPP)
 
 * Only provide scripts for calculating the approximation track.
 * 用 Docker Volume 存程式跑完的資料，等全部跑完以後再用 docker cp 指令存到本機。
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,26 +3,26 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import numpy as np
-from hicpapp import tools
+from hicpapp import papptools
 
 kwargs = {
-    "pearson":  "/home/jordan990301/Projects/HiCAPP/docker_volume_test/data/rao_2014/juicer_outputs/gm12878/1000000/pearsons/pearson_chr1.txt",
+    "pearson":  "/home/jordan990301/Projects/HiC-PAPP/docker_volume_test/data/rao_2014/juicer_outputs/gm12878/1000000/pearsons/pearson_chr1.txt",
     "zero_mean": True 
 }
-pearson_np = tools.read_pearson(**kwargs)
-approx_np = tools.create_approx(pearson_np)
-Vh, explained_variances, total_entry_num, valid_entry_num = tools.calc_explained_variance(pearson_np)
+pearson_np = papptools.read_pearson(**kwargs)
+approx_np = papptools.create_approx(pearson_np)
+Vh, explained_variances, total_entry_num, valid_entry_num = papptools.calc_explained_variance(pearson_np)
 
-print(tools.calc_correctness(pc1_np=Vh[0], approx_np=approx_np))
+print(papptools.calc_correctness(pc1_np=Vh[0], approx_np=approx_np))
 
 kwargs_2 = {
     "pc1_np": Vh[0],
     "approx_np": approx_np,
     "figsize": 20,
-    "scatter": "/home/jordan990301/Projects/HiCAPP/docker_volume_test/outputs/scatter.png",
-    "relative_magnitude": "/home/jordan990301/Projects/HiCAPP/docker_volume_test/outputs/line.png",
+    "scatter": "/home/jordan990301/Projects/HiC-PAPP/docker_volume_test/outputs/scatter.png",
+    "relative_magnitude": "/home/jordan990301/Projects/HiC-PAPP/docker_volume_test/outputs/line.png",
 }
-tools.plot_comparison(**kwargs_2)
+papptools.plot_comparison(**kwargs_2)
 
 ####
 kwargs = {
@@ -34,17 +34,22 @@ kwargs = {
     "method": "oe",
     "zero_mean": True 
 }
-pearson_np = tools.straw_to_pearson(**kwargs)
-approx_np = tools.create_approx(pearson_np)
-Vh, explained_variances, total_entry_num, valid_entry_num = tools.calc_explained_variance(pearson_np)
+pearson_np = papptools.straw_to_pearson(**kwargs)
+approx_np = papptools.create_approx(pearson_np)
+Vh, explained_variances, total_entry_num, valid_entry_num = papptools.calc_explained_variance(pearson_np)
 
-print(tools.calc_correctness(pc1_np=Vh[0], approx_np=approx_np))
+
+### Test
+print(papptools.calc_correctness(pc1_np=Vh[0], approx_np=approx_np))
+
+Vh[0] = -Vh[0]
+approx_np = -approx_np
 
 kwargs_2 = {
     "pc1_np": Vh[0],
     "approx_np": approx_np,
     "figsize": 20,
-    "scatter": "/home/jordan990301/Projects/HiCAPP/docker_volume_test/outputs/scatter2.png",
-    "relative_magnitude": "/home/jordan990301/Projects/HiCAPP/docker_volume_test/outputs/line2.png",
+    "scatter": "/home/jordan990301/Projects/HiC-PAPP/docker_volume_test/outputs/scatter2.png",
+    "relative_magnitude": "/home/jordan990301/Projects/HiC-PAPP/docker_volume_test/outputs/line2.png",
 }
-tools.plot_comparison(**kwargs_2)
+papptools.plot_comparison(**kwargs_2)
