f50174d3424df1eae33ddc05b0c6a08e3df37dd7
jordan990301 <jordan990301@gmail.com>
2024-05-30 14:37:15 +0800

    Add mem_profile.py which is used for the memory efficient algorithm for HiCPEP.

--- test/mem_profile.py
+++ test/mem_profile.py
@@ -7,25 +7,22 @@ from sklearn.decomposition import PCA
 from scipy import sparse
 np.set_printoptions(threshold=sys.maxsize)
 
-oe_path = "/media/jordan990301/Samsung_T5/HiC_Datasets/data_for_hicpap/data_store/data/lieberman_2009/heatmaps/HIC_gm06690_chr1_chr1_100000_obsexp.txt"
+def store_oe_sparse(oe_path):
+    oe_df = pd.read_table(oe_path, index_col=0, header=1, sep="\s+")
+    oe_np = oe_df.values
+    del oe_df
+    oe_np = oe_np.astype('float64')
+    diag = np.diag(oe_np)
+    diag_valid = diag != 0 
+    ixgrid = np.ix_(diag_valid, diag_valid) # Record the position of the valid sub-matrix.
+    oe_np = oe_np[ixgrid]
+    x = sparse.csr_matrix(oe_np)
+    sparse.save_npz('/tmp/oe_sparse.npz', x)
+    return
 
-# @profile
-def test():
-    A = np.array([
-        [1, 0, 2],
-        [0, 0, 3],
-        [4, 5, 6]
-    ])
-    # row = np.array([0, 0, 1, 2, 2, 2])
-    # col = np.array([0, 2, 2, 0, 1, 2])
-    # data = np.array([1, 2, 3, 4, 5, 6])
-    # sA = sparse.csr_matrix((data, (row, col)), shape=(3, 3))
-
-    nonzeros = A.nonzero()
-    sA = sparse.csr_matrix((A[nonzeros], nonzeros), shape=A.shape)
-    print(sA, '\n')
-    print(sA.toarray())
-
+def load_oe_sparse():
+    x = sparse.load_npz('/tmp/oe_sparse.npz')
+    print(x)
     return
 
 def sum_zero_percent(oe_path):
@@ -39,17 +36,7 @@ def sum_zero_percent(oe_path):
     return
 
 # @profile
-def normal(oe_path):
-    oe_df = pd.read_table(oe_path, index_col=0, header=1, sep="\s+")
-    oe_np = oe_df.values 
-    oe_np = oe_np.astype('float64')
-    del oe_df
-    pearson_np = np.corrcoef(oe_np)
-    peptools.create_est(pearson_np=pearson_np)
-    return
-
-# @profile
-def mem_efficient(oe_path):
+def test(oe_path):
     oe_df = pd.read_table(oe_path, index_col=0, header=1, sep="\s+")
     oe_np = oe_df.values
     del oe_df
@@ -57,39 +44,131 @@ def mem_efficient(oe_path):
     diag = np.diag(oe_np)
     diag_valid = diag != 0 
     ixgrid = np.ix_(diag_valid, diag_valid) # Record the position of the valid sub-matrix.
-    oe_np = oe_np[ixgrid] 
-    n = len(oe_np[0])
-    X = sparse.csr_matrix(oe_np)
-    cov = np.cov(oe_np, bias=True)
-    del oe_np
+    oe_np = oe_np[ixgrid]
+    corr = np.corrcoef(oe_np)
+    cov = np.cov(corr, bias=True)
+    print(f"test corr: {corr[0][:5]}")
+    print(f"test cov: {cov[0][:5]}")
+    return
+
+# @profile
+def mem_efficient_random1():
+    x = sparse.load_npz('/tmp/oe_sparse.npz')
     index_s = 0
-    I = np.full(n, 1)
     std = []
-    C = []
+    c = []
+    n = x.get_shape()[0]
 
     for i in range(n):
-        tmp = np.std(X[i].toarray())
+        tmp = np.std(x[i].toarray())
         std.append(tmp)
 
     for i in range(n):
-        tmp = X[i].mean()
-        C.append(tmp)
+        tmp = x[i].mean()
+        c.append(tmp)
 
-    v = (X[index_s].toarray() - X[index_s].mean())[0] # X selected.
     std = np.array(std)
-    C = np.array(C)
-    S = sparse.csr_matrix((1/std, np.arange(len(1/std)), np.arange(len(1/std) + 1)), shape=(len(1/std), len(1/std)))
-    X = X.dot(S)
-    r = C / std
-    corr_s = X.dot(v).flatten()
-    alpha_s = r @ v 
-    corr_s = (corr_s - I * alpha_s) / n
-    print(corr_s[:5])
+    c = np.array(c)
+    I = np.full(n, 1)
+    x_s = x[index_s].toarray()[0]
+    x_s -= x_s.mean()
+    alpha = I @ x_s
+    corr_s = (x.dot(x_s) - c * alpha) / ((std[index_s] * std) * n)
+    corr_s -= corr_s.mean()
+    cov_s = []
 
+    for i in range(n):
+        x_i = x[i].toarray()[0]
+        x_i -= x_i.mean()
+        alpha = I @ x_i
+        corr_i = (x.dot(x_i) - c * alpha) / ((std[i] * std) * n)
+        corr_i -= corr_i.mean()
+        entry = corr_i @ corr_s / n 
+        cov_s.append(entry)
+
+    cov_s = np.array(cov_s)
+
+    print(f"mem_efficient corr: {corr_s[:5]}")
+    print(f"mem_efficient cov: {cov_s[:5]}")
+    return
+
+@profile
+def normal(oe_path):
+    oe_df = pd.read_table(oe_path, index_col=0, header=1, sep="\s+")
+    oe_np = oe_df.values
+    del oe_df
+    oe_np = oe_np.astype('float64')
+    diag = np.diag(oe_np)
+    diag_valid = diag != 0 
+    ixgrid = np.ix_(diag_valid, diag_valid) # Record the position of the valid sub-matrix.
+    oe_np = oe_np[ixgrid]
+    pearson_np = np.corrcoef(oe_np)
+    est_np = peptools.create_est(pearson_np=pearson_np)
+
+    print(f"normal cov: {est_np[:5]}")
+    return
+
+@profile
+def mem_efficient_find_best():
+    x = sparse.load_npz('/tmp/oe_sparse.npz')
+    index_s = 0
+    std = []
+    c = []
+    n = x.get_shape()[0]
+
+    for i in range(n):
+        tmp = np.std(x[i].toarray())
+        std.append(tmp)
+
+    for i in range(n):
+        tmp = x[i].mean()
+        c.append(tmp)
+
+    std = np.array(std)
+    c = np.array(c)
+    I = np.full(n, 1)
+
+    max = 0
+    est_np = np.array([]) 
+
+    for k in range(n):
+        index_s = k # The target column (or row) in the Pearson's covariance matrix.
+        x_s = x[index_s].toarray()[0]
+        x_s -= x_s.mean()
+        alpha = I @ x_s
+        corr_s = (x.dot(x_s) - c * alpha) / ((std[index_s] * std) * n)
+        corr_s -= corr_s.mean()
+        cov_s = []
+
+        for i in range(n):
+            x_i = x[i].toarray()[0]
+            x_i -= x_i.mean()
+            alpha = I @ x_i
+            corr_i = (x.dot(x_i) - c * alpha) / ((std[i] * std) * n)
+            corr_i -= corr_i.mean()
+            entry = corr_i @ corr_s / n # Calculate each entry of the target column (or row) in the Pearson's covariance matrix.
+            cov_s.append(entry)
+
+        cov_s = np.array(cov_s)
+        track_sum_abs = np.sum(np.abs(cov_s))
+
+        if track_sum_abs > max:
+            max = track_sum_abs
+            est_np = cov_s.copy()
+
+    print(f"mem_efficient cov: {est_np[:5]}")
     return
 
 if __name__ == '__main__':
-    # test()
+    oe_path = "/media/jordan990301/Samsung_T5/HiC_Datasets/data_for_hicpap/data_store/data/lieberman_2009/heatmaps/HIC_gm06690_chr1_chr1_1000000_obsexp.txt"
+    # load_oe_sparse()
     # sum_zero_percent(oe_path)
-    # normal(oe_path)
-    mem_efficient(oe_path)
+
+    ####
+    # test(oe_path)
+    # mem_efficient_random1()
+    ####
+
+    store_oe_sparse(oe_path)
+    normal(oe_path)
+    mem_efficient_find_best()
